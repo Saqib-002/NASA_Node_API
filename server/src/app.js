577f314e9f46aba2d1af89bbdf897cb12d0847f5
Saqib-002 <saqibshah2882@gmail.com>
2023-03-31 06:04:43 +0500

    Serve frontend in production-2

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const path = require("path");
 const cors = require("cors");
+
 const planetsRouter = require("./routes/planets/planets.router");
 
 const app = express();
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "public")));
 app.use(
   cors({
     origin: "http://localhost:3000",
@@ -10,5 +15,8 @@ app.use(
 );
 
 app.use("/planets", planetsRouter);
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
 
 module.exports = app;
