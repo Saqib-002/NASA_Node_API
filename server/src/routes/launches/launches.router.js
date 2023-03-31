4eb5b0424bd6569855e58072f98b9dde51dc8a19
Saqib-002 <saqibshah2882@gmail.com>
2023-03-31 16:40:58 +0500

    Added Data Access Layer

--- server/src/routes/launches/launches.router.js
+++ server/src/routes/launches/launches.router.js
@@ -1,8 +1,8 @@
 const express = require("express");
-const { getAllLaunches } = require("./launches.controller");
+const { httpGetAllLaunches } = require("./launches.controller");
 
 const launchRouter = express.Router();
 
-launchRouter.get("/", getAllLaunches);
+launchRouter.get("/", httpGetAllLaunches);
 
 module.exports = launchRouter;
