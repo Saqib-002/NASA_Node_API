b666e36844dfb49265ea4a16843d3b0232e3ebe9
Saqib-002 <saqibshah2882@gmail.com>
2023-04-07 05:22:04 +0500

    sorted the paginated data

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -4,16 +4,6 @@ const axios = require("axios");
 
 const DEFAULT_FLIGHT_NUMBER = 100;
 
-const launch = {
-  flightNumber: 100,
-  mission: "Kepler Exploration X",
-  rocket: "Explore IS1",
-  launchDate: new Date("December 27, 2030"),
-  target: "Kepler-442 b",
-  customers: ["ZTM", "NASA"],
-  upcoming: true,
-  success: true,
-};
 async function getLatestFlightNumber() {
   const latestLaunch = await launches.findOne().sort("-flightNumber");
   if (!latestLaunch) {
@@ -33,7 +23,6 @@ async function saveLaunch(launch) {
     }
   );
 }
-saveLaunch(launch);
 async function scheduleNewLaunch(launch) {
   const planet = await planets.findOne({
     keplerName: launch.target,
@@ -60,6 +49,7 @@ async function getAllLaunches(skip, limit) {
         __v: 0,
       }
     )
+    .sort({ flightNumber: 1 })
     .skip(skip)
     .limit(limit);
 }
