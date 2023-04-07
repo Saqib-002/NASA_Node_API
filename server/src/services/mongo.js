264aa80c9ff57ab2ef78c90f2b49e0800ee39aa4
Saqib-002 <saqibshah2882@gmail.com>
2023-04-07 05:39:10 +0500

    manage secuirty with dotenv

--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 
-const MONGO_URL =
-  "mongodb+srv://nasa-api:YvRICWxb8JfUnSiu@nasa-cluster.bbgbw2s.mongodb.net/nasa?retryWrites=true&w=majority";
+const MONGO_URL = process.env.MONGO_URL;
 
 mongoose.connection.once("open", () => {
   console.log("connection is ready!!!");
